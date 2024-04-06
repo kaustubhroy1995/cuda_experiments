e03792c95ed7da451d099717253d9420a970313a
Kaustubh Roy <110053115+kaustubhroy1995@users.noreply.github.com>
2024-04-06 16:22:22 +0100

    Update cud_test1.cu
    minor modifications

--- cud_test1.cu
+++ cud_test1.cu
@@ -1,4 +1,12 @@
 #include <cuda.h>
 #include <cudnn.h>
 #include <cublas.h>
-#include <std.io>
+#include <stdio.h>
+
+int main(){
+  int* a = int[10];
+  while(i<10){
+    a[i] = i;
+    i+=1;
+  }
+}
